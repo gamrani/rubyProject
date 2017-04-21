4f7bb410bf5244a37f301a3b5d6291d695cb163f
gamrani <gamraniyoussef@gmail.com>
2017-04-21 19:34:08 +0100

    test version beta

--- adherent.rb
+++ adherent.rb
@@ -1,7 +1,15 @@
 class Adherent
   attr_accessor :nom , :prenom , :statut
 
-  
+  include Enum
+
+ def initialize(nom,prenom,statut)
+   @statut=statut
+   @nom=nom
+   @prenom=prenom
+
+   puts "#{@nom} #{@prenom} #{@statut}"
+ end
 
 
 end
